84d90141afae5f38704d38b447ba16fb6d1d448d
ryosagisu <31160828+ryosagisu@users.noreply.github.com>
2018-02-12 14:41:09 +0700

    Update fetchData.php

--- web/fetchData.php
+++ web/fetchData.php
@@ -1,31 +1,43 @@
 <?php
+//load kkni data set
 $kkni = file_get_contents("kkni.xml");
 $x = new DOMDocument();
 $x->loadXML($kkni);
 
+//load skkni data set
 $skkni = file_get_contents("skkni.xml");
 $y = new DOMDocument();
 $y->loadXML($skkni);
 
+//load peta_okupasi data set
 $peta_okupasi = file_get_contents("peta_okupasi.xml");
 $z = new DOMDocument();
-$z->loadXML($peta_okupasi); // Or load if filename required
+$z->loadXML($peta_okupasi);
+
+//compare data set with schema
 if ($x->schemaValidate("kkni.xsd") && $y->schemaValidate("skkni.xsd") && $z->schemaValidate("peta_okupasi.xsd")) {
-  // echo "Valid";
 
+  //TODO: turn domain input into array, user can choose more than one domain
   foreach ($x->getElementsByTagName("Bidang") as $node) {
-    if (validNodeKKNI($node)) {
-      $dataKKNI = shownode($node);
+    if (validNode($node, "domain", "Pengoperasian Komputer")) {
+      $dataKKNI = shownode($node, "KKNI", "1");
     }
   }
 
+  //TODO: it is possible to have more than one Jenjang
+  $listKompetensi = array();
+  foreach ($dataKKNI["Jenjang"][0]["UnitKompetensi"] as $value) {
+    array_push($listKompetensi, $value["kodeUnitKompetensi"][0]);
+  }
 
-  //TODO: use 'kodeUnitKompetensi' value from $dataKKNI and/or show only value of 'kodeUnitKompetensi' from $dataKKNI, currently showing all 'kodeUnit' from 'TujuanUtama' in SKKNI that contains one of 'kodeUnitKompetensi'
+  //read skkni data set and only fetch used 'kodeUnit'
   foreach ($y->getElementsByTagName("TujuanUtama") as $node) {
-    if (validNodeSKKNI($node)) {
-      $dataSKKNI = shownode($node);
+    if (validNode($node, "kodeUnit", $listKompetensi)) {
+      $dataSKKNI = shownode($node, "SKKNI", $listKompetensi);
     }
   }
+  // echo json_encode($listKompetensi);
+  // print_r($dataKKNI);
   echo json_encode($dataKKNI);
   echo "<br/><br/><br/>";
   echo json_encode($dataSKKNI);
@@ -33,24 +45,24 @@ if ($x->schemaValidate("kkni.xsd") && $y->schemaValidate("skkni.xsd") && $z->sch
   //TODO: write logic for peta_okupasi
 }
 
-function shownode($x) {
+function shownode($x, $root, $set="") {
   $i = 0;
   foreach ($x->childNodes as $p){
-    // echo $i++ . "<br/>";
-    // echo $p->nodeName;
     if ($p->nodeType != XML_ELEMENT_NODE) continue;
     if(!isset($result[$p->nodeName])) $result[$p->nodeName] = array();
 
     if (hasChild($p)) {
-      // echo $p->nodeName.' -> Nodes with child<br>';
-      // $temp = shownode($p);
-      // if(!isset($result[$p->parentNode->nodeName])) echo $p->parentNode->nodeName;
-      // $result = array_merge_recursive($result, shownode($p));
-      array_push($result[$p->nodeName], shownode($p));
+      if ($root == "KKNI" && $p->nodeName == "Jenjang") {
+        if(!validNode($p, "level", $set)) continue;
+      }
+
+      if ($root == "SKKNI" && $p->nodeName == "UnitKompetensi") {
+        if(!validNode($p, "kodeUnit", $set)) continue;
+      } 
+      array_push($result[$p->nodeName], shownode($p, $root, $set));     
     } 
     else{
       array_push($result[$p->nodeName], $p->nodeValue);
-      // echo $p->nodeName.' : '.$p->nodeValue.'<br>';
     }
   }
   return $result;
@@ -66,18 +78,7 @@ function hasChild($p) {
   return false;
 }
 
-//TODO: make this function dynamic
-function validNodeKKNI(DOMNode $domNode){
-  return $domNode->getElementsByTagName("domain")->item(0)->nodeValue == "Pengoperasian Komputer" && $domNode->getElementsByTagName("level")->item(0)->nodeValue == "1";
-}
-
-//TODO: make this function dynamic
-function validNodeSKKNI(DOMNode $domNode){
-  return $domNode->getElementsByTagName("kodeUnit")->item(0)->nodeValue == "J.63OPR00.001.2";
-}
-
-//TODO: make this function dynamic
-function validNodePetaOkupasi(DOMNode $domNode){
-  return $domNode->getElementsByTagName("domain")->item(0)->nodeValue == "Pengoperasian Komputer" && $domNode->getElementsByTagName("level")->item(0)->nodeValue == "1";
+function validNode(DOMNode $domNode, $tagName, $value){
+  return is_array($value) ? in_array($domNode->getElementsByTagName($tagName)->item(0)->nodeValue, $value) : $domNode->getElementsByTagName($tagName)->item(0)->nodeValue == $value;
 }
 ?>
